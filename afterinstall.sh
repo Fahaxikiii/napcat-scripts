cdb9ddf5e1f1805b21959f820ba798138109abcc
万里 <920113370213@njust.edu.cn>
2024-11-10 10:00:02 +0800

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -8,25 +8,25 @@ unzip -o NapCat.Shell.zip -d /opt/QQ/resources/app/app_launcher/napcat/
 
 rm -rf NapCat.Shell.zip
 
-log "正在备份并修改 /opt/QQ/resources/app/package.json..."
+echo "正在备份并修改 /opt/QQ/resources/app/package.json..."
 
 sudo cp -f /opt/QQ/resources/app/package.json /opt/QQ/resources/app/package.json.bak
 
 if sudo jq '.main = "./loadNapCat.js"' /opt/QQ/resources/app/package.json > ./package.json.tmp; then
     sudo mv ./package.json.tmp /opt/QQ/resources/app/package.json
-    log "修改QQ启动配置成功..."
+    echo "修改QQ启动配置成功..."
 else
-    log "修改失败，还原备份..."
+    echo "修改失败，还原备份..."
     sudo mv /opt/QQ/resources/app/package.json.bak /opt/QQ/resources/app/package.json
     exit 1
 fi
 
-log "正在修补文件..."
+echo "正在修补文件..."
 sudo echo "(async () => {await import('file:///opt/QQ/resources/app/app_launcher/napcat/napcat.mjs');})();" > /opt/QQ/resources/app/loadNapCat.js
 if [ $? -ne 0 ]; then
-    log "loadNapCat.js文件写入失败, 请以root身份运行。"
+    echo "loadNapCat.js文件写入失败, 请以root身份运行。"
     clean
     exit 1
 else
-    log "修补文件成功"
+    echo "修补文件成功"
 fi
